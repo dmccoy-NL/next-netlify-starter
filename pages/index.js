2b4a250e2771718f20d75e8e5f90b991394296c2
Devin <dmccoy@forte.io>
2024-06-24 15:16:16 -0400

    add imports

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
   const [initCode, setInitCode] = useState("");
 
